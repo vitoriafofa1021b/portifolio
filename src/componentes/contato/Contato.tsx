61bf6b9966bc192c07f4b000bb6747a6bf95a8fb
vitoriafofa1021b <vitoria.santos9@estudante.ifms.edu.br>
2023-10-17 11:03:09 -0400

    commit

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -10,7 +10,7 @@ function mudaEmail(e: React.ChangeEvent<HTMLInputElement>) {
   setEmail(e.target.value);
 }
 const [mensagem,setMensagem] = useState("");
-function mudaMensagem(e: React.ChangeEvent<HTMLInputElement>) {
+function mudaMensagem(e: React.ChangeEvent<HTMLTextAreaElement>) {
   setMensagem(e.target.value);
 }
 
@@ -22,24 +22,28 @@ function mudaMensagem(e: React.ChangeEvent<HTMLInputElement>) {
           <p>Envie-nos uma mensagem:</p>
           <form className="contato-form">
             <div className="form-group">
-              <label htmlFor="nome">Nome:</label>
-              <input type="text" id="nome" name="nome" placeholder="Seu nome"  onChange={mudaNome} required/>
+              <label htmlFor="nome">Nome</label>
+              <input type="text" id="nome" name="nome" placeholder="Seu nome"  onChange={mudaNome} required/> 
+               <p className="state">{nome}</p>
+
             </div>
             <div className="form-group">
-              <label htmlFor="email">Email:</label>
+              <label htmlFor="email">Email</label>
               <input type="email" id="email" name="email" placeholder="Seu email" onChange={mudaEmail} required/>
+              <p className="state">{email}</p>
             </div>
             <div className="form-group">
               <label htmlFor="mensagem">Mensagem</label>
               <textarea id="mensagem" name="mensagem" placeholder="Sua mensagem" onChange={mudaMensagem} required></textarea>
+              <p className="state">{mensagem}</p>
             </div>
       <button type="submit">Enviar</button>
           </form>
         </div>
         <div className="alinha">
-          <p className="state">Nome:{nome}</p>
-          <p className="state">Email:{email}</p>
-          <p className="state">Mensagem:{mensagem}</p>
+
+         
+         
         </div>
       </main>
     );
