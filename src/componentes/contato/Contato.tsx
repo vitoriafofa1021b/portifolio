32de11955ff252a2ed918a4942c18d579d941834
vitoriafofa1021b <vitoria.santos9@estudante.ifms.edu.br>
2023-10-17 08:30:41 -0400

    oi

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
 import './Contato.css'
 function Contato(){
+const [nome,setNome] = useState("");
+function mudaNome(e: React.ChangeEvent<HTMLInputElement>) {
+  setNome(e.target.value);
+}
+const [email,setEmail] = useState("");
+function mudaEmail(e: React.ChangeEvent<HTMLInputElement>) {
+  setEmail(e.target.value);
+}
+const [mensagem,setMensagem] = useState("");
+function mudaMensagem(e: React.ChangeEvent<HTMLInputElement>) {
+  setMensagem(e.target.value);
+}
+
+
     return(
         <main className="contato">
         <div className="contato-container">
@@ -8,20 +23,26 @@ function Contato(){
           <form className="contato-form">
             <div className="form-group">
               <label htmlFor="nome">Nome:</label>
-              <input type="text" id="nome" name="nome" placeholder="Seu nome" />
+              <input type="text" id="nome" name="nome" placeholder="Seu nome"  onChange={mudaNome} required/>
             </div>
             <div className="form-group">
               <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" placeholder="Seu email" />
+              <input type="email" id="email" name="email" placeholder="Seu email" onChange={mudaEmail} required/>
             </div>
             <div className="form-group">
               <label htmlFor="mensagem">Mensagem</label>
-              <textarea id="mensagem" name="mensagem" placeholder="Sua mensagem"></textarea>
+              <textarea id="mensagem" name="mensagem" placeholder="Sua mensagem" onChange={mudaMensagem} required></textarea>
             </div>
       <button type="submit">Enviar</button>
           </form>
+        </div>
+        <div className="alinha">
+          <p className="state">Nome:{nome}</p>
+          <p className="state">Email:{email}</p>
+          <p className="state">Mensagem:{mensagem}</p>
         </div>
       </main>
     );
   }
 export default Contato
+
